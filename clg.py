7a9c5d7dd22a916327b7f11aab9c36dbaad16efb
jungleman <124931697+the-jungleman@users.noreply.github.com>
2024-10-04 18:49:54 -0300

    Update clg.py

--- clg.py
+++ clg.py
@@ -1,19 +1,15 @@
-class Vector_shuffle_set:
-    def __init__(self,mux,increment,module):
-        self.mux=mux
-        self.increment=increment
-        self.module=module
+class   Vector_shuffle_set:
+    def __init__(self):
         self.mux=1664525
         self.increment=1013904223
         self.module=2**32
-        vector=[]
+        self.vector=[]
 
     def lcg(self,lenght,seed):
-        for i in range(self,lenght):
-            seed=(self.mux*seed+increment)%module
-            vector.append(seed)
-        return  vector
+        for i   in  range(lenght):
+            seed=(self.mux*seed+self.increment)%self.module
+            self.vector.append(seed)
+        return  self.vector
 
     def run_lcg(self,vector_len,initial_seed):
-        self.vector_len=vector_len
-        self.initial_seed=initial_seed
+        return  self.lcg(vector_len,initial_seed)
